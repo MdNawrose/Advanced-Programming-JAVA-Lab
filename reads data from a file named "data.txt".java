c8dd2726ed38816aadb785d67a47b401a3e7eed7
MD. NAWROSE <124398938+MdNawrose@users.noreply.github.com>
2024-09-13 02:14:18 +0600

    Java Lab Task
    Write a Java program that reads data from a file named "data.txt". Implement error handling using try-catch blocks to handle FileNotFoundException. If the file is not found, print an error message indicating the issue.

---
+++ reads data from a file named "data.txt".java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Question_01 {
+public static void main(String[] args) {
+String fileName = "C:\\data.txt";
+try {
+File file = new File(fileName);
+Scanner scanner = new Scanner(file);
+while (scanner.hasNextLine()) {
+String line = scanner.nextLine();
+System.out.println(line);
+}
+scanner.close();
+} catch (FileNotFoundException e) {
+System.err.println("Error: The file " + fileName + " was not
+found.");
+}
+}
+}
