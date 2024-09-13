c3e42f3aeb6d295d6dbf5a8246de8b397ae174ea
MD. NAWROSE <124398938+MdNawrose@users.noreply.github.com>
2024-09-14 02:07:56 +0600

    Java Lab Task
    Write a Java program that initializes an array of integers and attempts to access an element at an index beyond the array's length. Implement try-catch blocks to handle the ArrayIndexOutOfBoundsException that may occur. If the exception occurs, print a message indicating the invalid index.

---
+++ Initializes an array of integers and attempts to access an element.java
@@ -0,0 +1,14 @@
+public class Question_01 {
+public static void main(String[] args) {
+int[] numbers = {1, 2, 3, 4, 5};
+int Index = 10;
+try {
+int value = numbers[Index];
+System.out.println("Value at index " + Index + ": " + value);
+    }
+catch (ArrayIndexOutOfBoundsException e) {
+System.err.println("Error: " + Index + " is beyond the array's
+length.");
+    }
+  }
+}
