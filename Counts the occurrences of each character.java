cc0ef881ba1fdb91b8a1445b5345ff0fbc2ee16d
Md. Nawrose <124398938+MdNawrose@users.noreply.github.com>
2024-09-27 00:56:30 +0600

    Create Counts the occurrences of each character.java
    Write a java program that counts the occurrences of each character in a given string and displays the count for each character.

---
+++ Counts the occurrences of each character.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+public class CharacterCount {
+public static void main(String[] args) {
+String inputString = "hello world";
+countCharacters(inputString);
+}
+public static void countCharacters(String s) {
+HashMap<Character, Integer> charCount = new HashMap<>();
+for (char ch : s.toCharArray())
+ {
+if (charCount.containsKey(ch)) 
+{
+charCount.put(ch, charCount.get(ch) + 1);
+}
+else
+ {
+charCount.put(ch, 1);
+}
+}
+for (Map.Entry<Character, Integer> entry : charCount.entrySet()) {
+System.out.println("'" + entry.getKey() + "': " + entry.getValue());
+}
+}
+}
