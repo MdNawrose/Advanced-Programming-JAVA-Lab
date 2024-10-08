3c84ab50fbe62db7be0051e17e005f3f4ab061ba
Md. Nawrose <124398938+MdNawrose@users.noreply.github.com>
2024-10-08 11:49:59 +0600

    Java Lab Task

--- following strings.java
+++ following strings.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class StringManipulation {
+public class StringManipulation { 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
        System.out.print("Enter string A: ");
